598ec0a40786d0eb4423ac9b8956fe56e6957f6c
Fumitoshi Ogata <ogata.fumitoshi@o-12984-mac.local>
2019-11-08 20:06:45 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './index.css';
 import {Button, ButtonToolbar, Alert, Card} from 'react-bootstrap';
+import QrReader from 'react-qr-reader'
 
 import React, {
     useState,
@@ -16,8 +17,8 @@ import {
 //const { isRelayHubDeployedForRecipient, getRecipientFunds } = utils;
 
 const infuraToken = process.env.REACT_APP_INFURA_TOKEN || '95202223388e49f48b423ea50a70e336';
-const mWalletAddress = "0x9df5d4DD565fbFF51A6b72cD9156a7101C7e291E";
-const mPrivateKey = "0x97393BF2650FE37A1367DF85C0BE68EB1FE64A722D2938C14EFB8C245B5BAB61";
+//const mWalletAddress = "0x9df5d4DD565fbFF51A6b72cD9156a7101C7e291E";
+//const mPrivateKey = "0x97393BF2650FE37A1367DF85C0BE68EB1FE64A722D2938C14EFB8C245B5BAB61";
 const contractAddress = "0x97bac60e8f15d4a2afda72a0dd50ea04a7adcee9";
 const abi2 = '[{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"name","type":"string"},{"name":"symbol","type":"string"},{"name":"decimals","type":"uint8"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"sender","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"account","type":"address"},{"name":"amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"mintTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"context","type":"bytes"},{"name":"success","type":"bool"},{"name":"actualCharge","type":"uint256"},{"name":"preRetVal","type":"bytes32"}],"name":"postRelayedCall","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"context","type":"bytes"}],"name":"preRelayedCall","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"setRelayHubAddress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_initialAmount","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"oldRelayHub","type":"address"},{"indexed":true,"name":"newRelayHub","type":"address"}],"name":"RelayHubChanged","type":"event"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"},{"name":"","type":"bytes"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"bytes"},{"name":"","type":"uint256"}],"name":"acceptRelayedCall","outputs":[{"name":"","type":"uint256"},{"name":"","type":"bytes"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getHubAddr","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getRecipientBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"relayHubVersion","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]';
 const jsonContractAbi = JSON.parse(abi2);
@@ -25,14 +26,7 @@ var wa = "-----";
 var pk = "-----";
 
 function App() {
-    const signKey = useEphemeralKey();
-    const context = useWeb3Network(`wss://rinkeby.infura.io/ws/v3/${infuraToken}`, {
-        pollInterval: 15 * 1000,
-        gsn: {
-            signKey: mPrivateKey,
-        },
-    });
-    const instance = new context.lib.eth.Contract(jsonContractAbi, contractAddress);
+
 
     const strWalletAddress = localStorage.getItem('storage_wallet_address');
     const strWalletPk = localStorage.getItem('storage_wallet_pk');
@@ -41,6 +35,15 @@ function App() {
       wa = strWalletAddress;
       pk = strWalletPk;
     }
+
+    const signKey = useEphemeralKey();
+    const context = useWeb3Network(`wss://rinkeby.infura.io/ws/v3/${infuraToken}`, {
+        pollInterval: 15 * 1000,
+        gsn: {
+            signKey: pk,
+        },
+    });
+    const instance = new context.lib.eth.Contract(jsonContractAbi, contractAddress);
 
     const [count, setCount] = useState(1);
     const [walletAddress, setWalletAddress] = useState(wa);
@@ -91,6 +94,8 @@ function App() {
           pk = privateKey.toString("hex");
           localStorage.setItem('storage_wallet_address', wa);
           localStorage.setItem('storage_wallet_pk', pk);
+          //location.reload();
+          window.location.reload()
         } catch (e) {
             console.log(e);
         }
@@ -99,7 +104,7 @@ function App() {
     const sendMintTo = async address => {
         try {
             const tx = await instance.methods.mintTo(address, 1).send({
-                from: mWalletAddress
+                from: wa
             });
         } catch (e) {
             console.log(e);
@@ -150,6 +155,16 @@ function App() {
       getHubAddr(); 
     }
 
+/*
+  const handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+*/
+
     balanceOf(wa);
     getSymbol();
     getTotalSupply();
@@ -188,18 +203,18 @@ function App() {
 
 <Card style={{ width: '25rem' }}>
   <Card.Body>
-    <Card.Title> Title </Card.Title>
+    <Card.Title> Mission 1 </Card.Title>
     <Card.Text>
-      aaaaaaaaaaaa
+      This is Mission 1 
     </Card.Text>
     <Button onClick={mintToMyWallet}>CHECKIN</Button>
   </Card.Body>
 </Card>
 <Card style={{ width: '25rem' }}>
   <Card.Body>
-    <Card.Title> Title </Card.Title>
+    <Card.Title> Mission 2 </Card.Title>
     <Card.Text>
-      aaaaaaaaaaaa
+      This is Mission 1 
     </Card.Text>
     <Button onClick={mintToMyWallet}>CHECKIN</Button>
   </Card.Body>
@@ -207,7 +222,7 @@ function App() {
 </p>
 
 
-<p> <Button onClick={mintToMyWallet}>test</Button></p>
+
 <p> <Button onClick={removeLocalStorage}>removeLocalStorage</Button></p>
 
 
